b0963a3618f0020589423966f3ef865df408db0e
hemanttiwary <hemant.appu@gmail.com>
2023-11-04 11:53:54 +0530

    file modified

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 class test{
     public static void main(String args[]){
         System.out.println("This java file is for git Repo");
+        System.out.println("Try to learn git and github as quickas possible.....");
     }
 
 }

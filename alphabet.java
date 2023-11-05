e4940ba4d5a09f74d311a495118eea88d3d6ed48
hemanttiwary <hemant.appu@gmail.com>
2023-11-05 12:44:10 +0530

    New Files added

---
+++ alphabet.java
@@ -0,0 +1,10 @@
+public class alphabet {
+    public static void main(String[] args) {
+       int i = 0;
+                   
+        for(i=65;i<=122; i++){
+            System.out.println(i + " => " + (char)i);
+        }
+    }
+    
+}
---
+++ num.java
@@ -0,0 +1,11 @@
+public class num {
+    public static void main(String args[]){
+        int i = 0;
+        for(i =1; i<=100; i++){
+             System.out.println(i);
+        }
+
+    }
+    
+}
+// to print output of 1 to 100 numbers using loop...
